2e3f0a975fea43516b2c122d2121fe415c0e21fd
Allen <almcbroombey@hotmail.com>
2022-07-21 22:19:20 -0400

    added syntax to PortfolioContainer.js

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -10,3 +10,33 @@ import Resume from "./pages/Resume";
 import Home from "./pages/Home";
 // load an array of projects
 import projects from "../projects";
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState("Home");
+    const renderPage = () => {
+        // based on the page clicked, load the appropriate component
+        switch (currentPage) {
+        case "About":
+            return <About />;
+        case "Contact":
+            return <Contact />;
+        case "Portfolio":
+            return <Portfolio projects={projects} />;
+        case "Resume":
+            return <Resume />;
+        default:
+            
+            return <Home />;
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+    return (
+        <div className="cover-container d-flex w-100 h-100 flex-column">
+        {/* show the Nav and Footer components regardless of the "main" component shown */}
+        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()}
+        <Footer />
+        </div>
+    );
+    }
