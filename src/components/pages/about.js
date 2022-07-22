fde8d64b37e0707b8ee63896c4212197ef7b14ab
Allen <almcbroombey@hotmail.com>
2022-07-21 21:39:38 -0400

    added syntax to about, contact, home

---
+++ src/components/pages/about.js
@@ -0,0 +1,29 @@
+import React from "react";
+import img from "../../img/AllenProfileImg.jpg";
+
+// this component contains an image and a short description
+export default function About() {
+    return (
+      <div className="container mt-5 animate__animated animate__fadeIn">
+        <div className="row justify-content-around">
+          <div className="col-12 col-md-6 p-5">
+            <div className="text-center">
+              <img
+                src={img}
+                className="rounded-circle img-fluid img-thumbnail w-auto"
+                alt="portfolio owner"
+              ></img>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 align-self-center p-3">
+            <p className="fs-3">
+              An accomplished and fluent communicator with strong investigation,
+              problem-solving and decision-making skills enhanced by previous
+              application support and development roles. Excited to transition
+              into a new career as a full-stack web developer.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
---
+++ src/components/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Home component just contains a header and is the first page the user would see when visiting the site
+export default function Home() {
+  return (
+    <div className="container mt-5 animate__animated animate__fadeIn">
+      <h1 className="display-2 text-center py-5">
+        Greetings, I am Allen McBroom Bey
+      </h1>
+      <h1 className="display-4 text-center py-3">
+        I am a Full-Stack Web Developer
+      </h1>
+    </div>
+  );
+}
