61eba550794f62740d583427ba6209d9868b04ef
Allen <almcbroombey@hotmail.com>
2022-07-22 00:31:42 -0400

    added syntax to projects.js

---
+++ src/projects.js
@@ -0,0 +1,132 @@
+import img1 from "";
+import img2 from "";
+import img3 from "";
+import img4 from "";
+import img5 from "";
+import img6 from "";
+
+
+const Projects = [
+    {
+        id: 1,
+        name: "Daily-Weather-Forecast-Dashboard",
+        github: "https://github.com/AllenM03/Daily-Weather-Forecast-Dashboard",
+        site: "https://allenm03.github.io/Daily-Weather-Forecast-Dashboard/",
+        description: `This project is a weather dashboard app with search functionality to find 
+        current weather conditions and forecasted weather for multiple cities. 
+        It uses the OpenWeather API to retrieve data for the cities, AJAX to hook into the API 
+        to retrieve the data in JSON format, dynamically updated HTML and CSS powered by 
+        jQuery, and displays a search history which the user can click to access past 
+        cities searched via local storage.
+
+        This weather dashboard shows the weather conditions in real time along with a 
+        five day forecast when you enter the city in the search bar.
+        
+        A few different files were used in creating this application such 
+        as, HTML, CSS, and JavaScript powered by third-party API's such as 
+        Moment.js, Bootstrap, OpenWeather's GeoCoding and Ond Cal API's.`
+        imgSrc: img1,
+    },
+    {
+        id: 2,
+        name: "Daily-Weather-Forecast-Dashboard",
+        github: "https://github.com/AllenM03/Daily-Weather-Forecast-Dashboard",
+        site: "https://allenm03.github.io/Daily-Weather-Forecast-Dashboard/",
+        description: `This project is a weather dashboard app with search functionality to find 
+        current weather conditions and forecasted weather for multiple cities. 
+        It uses the OpenWeather API to retrieve data for the cities, AJAX to hook into the API 
+        to retrieve the data in JSON format, dynamically updated HTML and CSS powered by 
+        jQuery, and displays a search history which the user can click to access past 
+        cities searched via local storage.
+
+        This weather dashboard shows the weather conditions in real time along with a 
+        five day forecast when you enter the city in the search bar.
+        
+        A few different files were used in creating this application such 
+        as, HTML, CSS, and JavaScript powered by third-party API's such as 
+        Moment.js, Bootstrap, OpenWeather's GeoCoding and Ond Cal API's.`
+        imgSrc: img2,
+    },
+    {
+        id: 3,
+        name: "Daily-Weather-Forecast-Dashboard",
+        github: "https://github.com/AllenM03/Daily-Weather-Forecast-Dashboard",
+        site: "https://allenm03.github.io/Daily-Weather-Forecast-Dashboard/",
+        description: `This project is a weather dashboard app with search functionality to find 
+        current weather conditions and forecasted weather for multiple cities. 
+        It uses the OpenWeather API to retrieve data for the cities, AJAX to hook into the API 
+        to retrieve the data in JSON format, dynamically updated HTML and CSS powered by 
+        jQuery, and displays a search history which the user can click to access past 
+        cities searched via local storage.
+
+        This weather dashboard shows the weather conditions in real time along with a 
+        five day forecast when you enter the city in the search bar.
+        
+        A few different files were used in creating this application such 
+        as, HTML, CSS, and JavaScript powered by third-party API's such as 
+        Moment.js, Bootstrap, OpenWeather's GeoCoding and Ond Cal API's.`
+        imgSrc: img3,
+    },
+    {
+        id: 4,
+        name: "Daily-Weather-Forecast-Dashboard",
+        github: "https://github.com/AllenM03/Daily-Weather-Forecast-Dashboard",
+        site: "https://allenm03.github.io/Daily-Weather-Forecast-Dashboard/",
+        description: `This project is a weather dashboard app with search functionality to find 
+        current weather conditions and forecasted weather for multiple cities. 
+        It uses the OpenWeather API to retrieve data for the cities, AJAX to hook into the API 
+        to retrieve the data in JSON format, dynamically updated HTML and CSS powered by 
+        jQuery, and displays a search history which the user can click to access past 
+        cities searched via local storage.
+
+        This weather dashboard shows the weather conditions in real time along with a 
+        five day forecast when you enter the city in the search bar.
+        
+        A few different files were used in creating this application such 
+        as, HTML, CSS, and JavaScript powered by third-party API's such as 
+        Moment.js, Bootstrap, OpenWeather's GeoCoding and Ond Cal API's.`
+        imgSrc: img4,
+    },
+    {
+        id: 5,
+        name: "Daily-Weather-Forecast-Dashboard",
+        github: "https://github.com/AllenM03/Daily-Weather-Forecast-Dashboard",
+        site: "https://allenm03.github.io/Daily-Weather-Forecast-Dashboard/",
+        description: `This project is a weather dashboard app with search functionality to find 
+        current weather conditions and forecasted weather for multiple cities. 
+        It uses the OpenWeather API to retrieve data for the cities, AJAX to hook into the API 
+        to retrieve the data in JSON format, dynamically updated HTML and CSS powered by 
+        jQuery, and displays a search history which the user can click to access past 
+        cities searched via local storage.
+
+        This weather dashboard shows the weather conditions in real time along with a 
+        five day forecast when you enter the city in the search bar.
+        
+        A few different files were used in creating this application such 
+        as, HTML, CSS, and JavaScript powered by third-party API's such as 
+        Moment.js, Bootstrap, OpenWeather's GeoCoding and Ond Cal API's.`
+        imgSrc: img5,
+    },
+    {
+        id: 6,
+        name: "Daily-Weather-Forecast-Dashboard",
+        github: "https://github.com/AllenM03/Daily-Weather-Forecast-Dashboard",
+        site: "https://allenm03.github.io/Daily-Weather-Forecast-Dashboard/",
+        description: `This project is a weather dashboard app with search functionality to find 
+        current weather conditions and forecasted weather for multiple cities. 
+        It uses the OpenWeather API to retrieve data for the cities, AJAX to hook into the API 
+        to retrieve the data in JSON format, dynamically updated HTML and CSS powered by 
+        jQuery, and displays a search history which the user can click to access past 
+        cities searched via local storage.
+
+        This weather dashboard shows the weather conditions in real time along with a 
+        five day forecast when you enter the city in the search bar.
+        
+        A few different files were used in creating this application such 
+        as, HTML, CSS, and JavaScript powered by third-party API's such as 
+        Moment.js, Bootstrap, OpenWeather's GeoCoding and Ond Cal API's.`
+        imgSrc: img6,
+    },
+];
+
+export default projects;
