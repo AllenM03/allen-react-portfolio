ccd2e7c6e217bb84ca800bfc26a0dc1e2741170d
Allen <almcbroombey@hotmail.com>
2022-07-23 04:46:31 -0400

    added resume sytax

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,3 +1,17 @@
 import React, { useState } from "react";
 // import the email validation checker
 import { validateEmail } from "../../utils/validateText";
+
+export default function Contact() {
+    //Checking if the values entered on all the fields as useState
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    // this event handler would store what is entered in the form
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
--- src/components/project.js
+++ src/components/project.js
@@ -10,4 +10,33 @@ export default function Project({ project }) {
   return (
     // each project is a card that contains an image, links to both Github repository and/or published web app and a description.
     <div className="col">
-      <div className="card mb-3"></div>
+    <div className="card mb-3">
+      <img
+        src={project.imgSrc}
+        className="img-fluid rounded-top"
+        alt="..."
+        style={imgStyle}
+      ></img>
+      <br></br>
+      <a
+        href={project.github}
+        className="d-flex justify-content-center text-decoration-none"
+        rel="noopener"
+      >
+        Github Repository
+      </a>
+      <a
+        href={project.site}
+        className="d-flex justify-content-center text-decoration-none"
+        rel="noopener"
+      >
+        {project.name}
+      </a>
+      <div className="card-body">
+        <h5 className="card-title text-dark">{project.name}</h5>
+        <p className="card-text text-dark">{project.description}</p>
+      </div>
+    </div>
+  </div>
+);
+}
